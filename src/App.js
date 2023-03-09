5a342d2522f3746c2d6e9c1a57e16acbcedc766d
sondule <tejassondule69@gmail.com>
2023-03-09 17:59:34 +0530

    submission

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import main from './main';
+
+function App() {
+  return (
+    <div className="App">
+          <main color={"black"} code={"#000000"} name={"Black"}/>
+
+          <main color={"red"} code={"#D0312D"} name={"Red"}/>
+
+          <main color={"BurlyWood "} code={"#DEB887"} name={"Burly Wood"}/>
+
+          <main color={"Cyan "} code={"	#00FFFF"} name={"Cyan "} />
+
+          <main color={"darkMagenta"} code={"#8B008B"} name={"Dark Magenta"}/>
+
+          <main color={"Pink"} code={"#FF1493e"}
+          name={"Pink"} />
+
+          <main color={"blue"} code={"#00BFFF"} name={"Blue"}/>
+
+         <main color={"DimGray"} code={"#696969"} name={"Dim Gary"}/>
+
+         <main color={"Indigo"} code={"#4B0082"} name={"Indigo"}/>
+
+         <main color={"GreenYellow"} code={"#ADFF2F"} name={"Green Yellow"}/>
+         
+
+    </div>
+  );
+}
+
+export default App;
